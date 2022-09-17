d97c6d05a1c17c22ec0902fc3af68ade5df8dff3
suhuq007@gmail.com <suhuq007@gmail.com>
2022-09-17 17:37:52 +0530

    initial commit

---
+++ src/logistics.js
@@ -0,0 +1,27 @@
+const myChart = document.getElementById('myC');
+
+export const chart = new Chart(myChart, {
+  type: 'line',
+  data: {
+    labels: [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'],
+    datasets: [{
+      label: 'My first dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: '#000',
+      data: [0, 10, 5, 2, 20, 35, 30, 45, 50, 55, 60],
+    }],
+  },
+  options: {}
+})
